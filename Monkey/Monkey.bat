5be5fb40931108ba4f8fcf7e28fc5e66ceb552b6
chad.long <chad.long@vipshop.com>
2016-01-18 23:34:01 +0800

    modify the monkey file

--- Monkey/Monkey.bat
+++ Monkey/Monkey.bat
@@ -6,6 +6,11 @@ rem adb devices
 rem adb shell
 rem opendir failed ,permission denied -> su命令回车，手机同意root
 rem cd data/data
-rem -v behind --throttle 
-adb shell monkey -p com.achievo.vipshop --throttle 500 -v 200 --pct-touch 80 --pct-motion 20 --ignore-crashes --ignore-timeouts --ignore-security-exception > E:\Project\git\Android_Testing\Monkey\log.txt
+rem -v behind --throttle
+@echo current directory
+dir
+@echo starting monkey...
+rem adb shell monkey -p com.achievo.vipshop --pct-touch 50 --pct-motion 20 --pct-trackball 0 --pct-nav 0 --pct-majornav 10 --pct-appswitch 10 --pct-anyevent 10 -s 12867 -v --throttle 456 2000 --ignore-crashes --ignore-timeouts --ignore-security-exception > log.txt
+adb shell monkey -p com.achievo.vipshop -s 12867 --throttle 456 -v 2000 --pct-touch 50 --pct-motion 20 --pct-trackball 0 --pct-nav 0 --pct-appswitch 10 --pct-anyevent 10 --ignore-crashes --ignore-timeouts --ignore-security-exception > log.txt
+
 rem adb shell monkey -help
