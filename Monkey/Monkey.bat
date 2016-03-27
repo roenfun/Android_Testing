371993f4bec4f1f9cfbb8976cef79493bf26b041
chad.long <chad.long@vipshop.com>
2016-03-27 22:24:44 +0800

    修改monkey文件

--- Monkey/Monkey.bat
+++ Monkey/Monkey.bat
@@ -1,3 +1,4 @@
+@echo off
 rem #######CI 配置##########
 rem cd D:\CI\artifact
 rem del testing-*
@@ -18,8 +19,13 @@ rem adb shell
 rem opendir failed ,permission denied -> su命令回车，手机同意root
 rem cd data/data
 rem -v behind --throttle
+
 @echo current directory
 dir
+@echo restart adb...
+adb kill-server
+adb start-server
+adb devices
 @echo starting monkey...
 rem adb shell monkey -p com.achievo.vipshop --pct-touch 50 --pct-motion 20 --pct-trackball 0 --pct-nav 0 --pct-majornav 10 --pct-appswitch 10 --pct-anyevent 10 -s 12867 -v --throttle 456 2000 --ignore-crashes --ignore-timeouts --ignore-security-exception > log.txt
 adb shell monkey -p com.achievo.vipshop -s 12867 --throttle 456 -v 2000 --pct-touch 50 --pct-motion 20 --pct-trackball 0 --pct-nav 0 --pct-appswitch 10 --pct-anyevent 10 --ignore-crashes --ignore-timeouts --ignore-security-exception > log.txt
