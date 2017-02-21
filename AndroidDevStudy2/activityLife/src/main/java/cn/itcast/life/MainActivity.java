59c128236d7fa3611df3782780343a3fe60a3e9b
chad.long <chad.long@vipshop.com>
2017-02-21 19:58:36 +0800

    Modify：提交某些配置

--- AndroidDevStudy2/activityLife/src/main/java/cn/itcast/life/MainActivity.java
+++ AndroidDevStudy2/activityLife/src/main/java/cn/itcast/life/MainActivity.java
@@ -17,7 +17,8 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        Log.i(TAG, "onCreate()");
+        Log.i(TAG, "<<----------------- onCreate() 首次创建 Activity 时调用,系统向此方法传递一个 Bundle 对象，" +
+				"其中包含 Activity 的上一状态，不过前提是捕获了该状态,始终后接 onStart()-------------->>");
     }
     
     public void openPauseActivity(View v){
@@ -30,37 +31,42 @@ public class MainActivity extends Activity {
 
 	@Override
 	protected void onStart() {
-		Log.i(TAG, "onStart()");
+		Log.i(TAG, "<<----------------- onStart() 在 Activity 即将对用户可见之前调用。" +
+				"如果 Activity 转入前台，则后接 onResume()，如果 Activity 转入隐藏状态，则后接 onStop()-------------->>");
 		super.onStart();
 	}
 
 	@Override
 	protected void onRestart() {
-		Log.i(TAG, "onRestart()");
+		Log.i(TAG, "<<----------------- onRestart() 在 Activity 已停止并即将再次启动前调用。" +
+				"始终后接 onStart()-------------->>");
 		super.onRestart();
 	}
 
 	@Override
 	protected void onResume() {
-		Log.i(TAG, "onResume()");
+		Log.i(TAG, "<<----------------- onResume() 在 Activity 即将开始与用户进行交互之前调用。 此时，Activity 处于 Activity 堆栈的顶层，并具有用户输入焦点。" +
+				"始终后接 onPause()-------------->>");
 		super.onResume();
 	}
 
 	@Override
 	protected void onStop() {
-		Log.i(TAG, "onStop()");
+		Log.i(TAG, "<<----------------- onStop() 在 Activity 对用户不再可见时调用。如果 Activity 被销毁，" +
+				"或另一个 Activity（一个现有 Activity 或新 Activity）继续执行并将其覆盖，就可能发生这种情况-------------->>");
 		super.onStop();
 	}
 
 	@Override
 	protected void onPause() {
-		Log.i(TAG, "onPause()");
+		Log.i(TAG, "<<----------------- onPause() 当系统即将开始继续另一个 Activity 时调用,如果 Activity 返回前台，则后接 onResume()，" +
+				"如果 Activity 转入对用户不可见状态，则后接 onStop()。-------------->>");
 		super.onPause();
 	}
 
 	@Override
 	protected void onDestroy() {
-		Log.i(TAG, "onDestroy()");
+		Log.i(TAG, "<<----------------- onDestroy() 在 Activity 被销毁前调用,释放所有的资源-------------->>");
 		super.onDestroy();
 	}
     
