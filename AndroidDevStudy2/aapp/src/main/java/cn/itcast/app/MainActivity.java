0b0bc684489fc4b2f6cdd08bd38da3efe6e5a09b
Chad.long <Chad.long@vipshop.com>
2017-03-29 13:41:06 +0800

    Modify：添加service的例子

--- AndroidDevStudy2/aapp/src/main/java/cn/itcast/app/MainActivity.java
+++ AndroidDevStudy2/aapp/src/main/java/cn/itcast/app/MainActivity.java
@@ -1,31 +1,91 @@
 package cn.itcast.app;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+
+import cn.itcast.sevice.MyService;
 
 /**
-*21_监听ContentProvider中数据的变化.avi
-*所在项目：db & other & Aapp
+ * 21_监听ContentProvider中数据的变化.avi
+ * 所在项目：db & other & Aapp
  */
-public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
+public class MainActivity extends Activity implements View.OnClickListener {
+    private Button startService, stopService, bindService, unbindService;
+    private MyService.MyBinder myBinder;
+
+    private ServiceConnection connection = new ServiceConnection() {
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+        }
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            myBinder = (MyService.MyBinder) service;
+            myBinder.startDownload();
+        }
+    };
+    /**
+     * Called when the activity is first created.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        Log.d("MyService", "MainActivity thread id is " + Thread.currentThread().getId());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        startService = (Button) findViewById(R.id.start_service);
+        stopService = (Button) findViewById(R.id.stop_service);
+        bindService = (Button) findViewById(R.id.bind_service);
+        unbindService = (Button) findViewById(R.id.unbind_service);
+
+        startService.setOnClickListener(this);
+        stopService.setOnClickListener(this);
+        bindService.setOnClickListener(this);
+        unbindService.setOnClickListener(this);
     }
-    
-    public void insert(View v){
-    	Uri uri = Uri.parse("content://cn.itcast.providers.personprovider/person");
-		ContentResolver resolver = this.getContentResolver();
-		ContentValues values = new ContentValues();
-		values.put("name", "A-App");
-		values.put("phone", "18607687688");
-		values.put("amount", "700000");
-		resolver.insert(uri, values);
+
+    public void insert(View v) {
+        Uri uri = Uri.parse("content://cn.itcast.providers.personprovider/person");
+        ContentResolver resolver = this.getContentResolver();
+        ContentValues values = new ContentValues();
+        values.put("name", "A-App");
+        values.put("phone", "18607687688");
+        values.put("amount", "700000");
+        resolver.insert(uri, values);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.start_service:
+                Intent startIntent = new Intent(this, MyService.class);
+                startService(startIntent);
+                break;
+            case R.id.stop_service:
+                Log.d("MyService", "click Stop Service button");
+                Intent stopIntent = new Intent(this, MyService.class);
+                stopService(stopIntent);
+                break;
+            case R.id.bind_service:
+                Intent bindIntent = new Intent(this, MyService.class);
+                bindService(bindIntent, connection, BIND_AUTO_CREATE);
+                break;
+            case R.id.unbind_service:
+                Log.d("MyService", "click Unbind Service button");
+                unbindService(connection);
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ AndroidDevStudy2/aapp/src/main/java/cn/itcast/sevice/MyService.java
@@ -0,0 +1,80 @@
+package cn.itcast.sevice;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import cn.itcast.app.MainActivity;
+import cn.itcast.app.R;
+
+/**
+ * Created by chad on 2017/3/28.
+ */
+
+public class MyService extends Service {
+    private static final String TAG = "MyService";
+    private MyBinder myBinder = new MyBinder();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("MyService", "MyService thread id is " + Thread.currentThread().getId());
+
+        //创建一个前台Service
+        Notification notification = new Notification(R.drawable.ic_launcher, "有通知到来", System.currentTimeMillis());
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+        notification.setLatestEventInfo(this, "这是通知的标题", "这是通知的内容",
+                pendingIntent);
+        startForeground(1, notification); //变为前台应用
+
+        Log.d(TAG, "onCreate() executed!");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        //一个比较标准的Service就可以写成：1)
+        Log.d(TAG, "onStartCommand() executed!");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // 开始执行后台任务
+            }
+        }).start();
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        //始终记得在Service的onDestroy()方法里去清理掉那些不再使用的资源，防止在Service被销毁后还会有一些不再使用的对象仍占用着内存
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() executed!");
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    public class MyBinder extends Binder {
+        public void startDownload() {
+            //一个比较标准的Service就可以写成：2)
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    // 执行具体的下载任务
+                }
+            }).start();
+
+            Log.d("TAG", "startDownload() executed");
+        }
+
+    }
+}
