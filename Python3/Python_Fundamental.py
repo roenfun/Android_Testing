060c4c2b476b0f9b1fb4d53e3a2c1346418b7c05
chad.long <chad.long@vipshop.com>
2019-01-22 11:13:09 +0800

    Add:Python Learning beginning

---
+++ Python3/Python_Fundamental.py
@@ -0,0 +1,9 @@
+#!/Library/Frameworks/Python.framework/Versions/3.7/bin
+
+#查看版本
+#python -V
+#Python3.7 -V
+
+#Mac安装Python3.x步骤
+#Mac系统自带python路径为: /System/Library/Frameworks/Python.framework/Versions
+#官网下载安装后的路径为：  /Library/Frameworks/Python.framework/Versions, 安装完毕后自动在.bash_profile文件里添加环境变量，也可以用brew install Python3 命令安装
