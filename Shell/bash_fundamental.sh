a0ce8e1b7c8e70533be004b9b58533714ed6c9f1
chad.long <chad.long@vipshop.com>
2019-03-05 14:03:24 +0800

    modify bash file

--- Shell/bash_fundamental.sh
+++ Shell/bash_fundamental.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #`chmod +x /Users/chad/project/git/Android_Testing/Shell/bash_fundamental.sh`  #使脚本具有执行权限
-#`ls /etc`
+#`ls -al`
 
 #注释
-your_name="qinjx"
+your_name="罗纳尔多"
 echo $your_name
 echo ${your_name}
