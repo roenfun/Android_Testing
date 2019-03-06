e93c5af3efc951702cda640c591d71e505b96bc8
Chad.long <Chad.long@vipshop.com>
2019-03-06 10:07:09 +0800

    Moidfy:添加bash修改提交

--- Shell/bash_fundamental.sh
+++ Shell/bash_fundamental.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
 #`chmod +x /Users/chad/project/git/Android_Testing/Shell/bash_fundamental.sh`  #使脚本具有执行权限
-#`ls -al`
+a=`which python`
+echo $a
 
 #注释
-your_name="罗纳尔多"
-echo $your_name
+your_name="R 罗纳尔多" #双引号可以引用变量
+my_name='欧文 $your_name'
+my_name1="欧文 ${your_name} _suff"
 echo ${your_name}
+echo $my_name
+echo $my_name1
+
+#预定义变量
+variabl=$PWD #$USER $ENV
+echo $variabl
+
+#数组
+array=(1 2 3 4 5)
+echo $array
+echo ${array[@]} #打印所有数组
+echo ${array[*]} #打印所有数组
+echo ${#array[@]} #数组长度
+echo ${#array[*]} #数组长度
