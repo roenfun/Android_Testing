02cafb52623cbd15115258f21f111396df1732b5
Chad.long <Chad.long@vipshop.com>
2019-03-13 09:58:51 +0800

    Modify:修改bash

--- Shell/bash_fundamental.sh
+++ Shell/bash_fundamental.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #`chmod +x /Users/chad/project/git/Android_Testing/Shell/bash_fundamental.sh`  #使脚本具有执行权限
-a=`which python`
+a=`which python` #等效于: $(which python)
 echo $a
 
 #注释
@@ -17,9 +17,34 @@ variabl=$PWD #$USER $ENV
 echo $variabl
 
 #数组
-array=(1 2 3 4 5)
+echo "--------------数组---------------"
+array=(1 "2" 3 4 5)
 echo $array
+array[1]="改变数组第二个元素的值"
+echo ${array[-1]} #$array[4] 不一样，要注意
 echo ${array[@]} #打印所有数组
 echo ${array[*]} #打印所有数组
-echo ${#array[@]} #数组长度
-echo ${#array[*]} #数组长度
+echo ${#array[@]} #数组长度length
+echo ${#array[*]} #数组长度size
+
+#传递参数 
+# http://www.runoob.com/linux/linux-shell-passing-arguments.html
+echo "--------------传递参数---------------"
+#if(0 eq $#)
+	echo "传递的参数总数是：$#, 参数总和：$*, 分别为：$0, $1, $2"
+#fi
+
+#运算符加减乘除
+# echo ${array[3]} + ${array[4]}
+val=$(expr 99 + 100) #语法很严格
+add=1;add2=3;trueT=true
+chu=`awk 'BEGIN{print 2/3}'`
+
+echo ${val}
+echo "add+add2=$((add=add+add2))"
+echo "trueT is: $trueT"
+echo "2*4=$((2*4))"
+echo $? #判断上一命令是true或false
+echo $chu
+
+#字符串应用切片
